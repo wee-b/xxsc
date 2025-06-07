74d070449dd0ed8911ab6ab0589f3262b9bafda8
wee <19064060958@163.com>
2025-06-07 21:51:13 +0800

    登录实现(无axios拦截器)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import '@/styles/common.scss'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { componentPlugin } from '@/components'
@@ -7,12 +7,16 @@ import App from './App.vue'
 import router from './router'
 import {lazyPlugin} from "@/directives/index.js";
 
-
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+// 应用持久化插件到pinia实例
+pinia.use(piniaPluginPersistedstate)
+
+// 使用已经配置好的pinia实例
+app.use(pinia)
 app.use(router)
-//实现图片懒加载
+// 实现图片懒加载
 app.use(lazyPlugin)
 app.use(componentPlugin)
 app.mount('#app')
